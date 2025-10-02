799824b265bc09922ec564abc5472cb70f692c8a
uzakotim <timauzakov@gmail.com>
2025-10-02 14:04:23 +0500

    init commit with light dark modes

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { ThemeToggle } from "./_components/ThemeToggle";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      <Button>Button</Button>
+      <ThemeToggle/>
+    </div> 
+  );
+}
